909df8d0782dbb987502fc3c5d497862fcd5bcb4
Lucas <lucas.niquele.ln@gmail.com>
2026-04-12 13:54:11 -0300

    fix: correct duplicated code in RestaurantMeta

--- frontend/client/src/components/RestaurantMeta.tsx
+++ frontend/client/src/components/RestaurantMeta.tsx
@@ -54,31 +54,4 @@ const RestaurantMeta: React.FC<RestaurantMetaProps> = ({ restaurant }) => {
   );
 };
 
-  const absoluteLogoUrl = getAbsoluteUrl(restaurant.logoUrl || '');
-
-  return (
-    <Helmet>
-      <title>{restaurant.name} | Cardápio Digital</title>
-      <meta name="description" content={`Peça agora no ${restaurant.name}. Confira nosso cardápio completo!`} />
-      
-      {/* Open Graph / Facebook / WhatsApp */}
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content={`${restaurant.name} - Cardápio Online`} />
-      <meta property="og:description" content="Faça seu pedido online de forma rápida e prática e receba em casa." />
-      {absoluteLogoUrl && <meta property="og:image" content={absoluteLogoUrl} />}
-      <meta property="og:image:width" content="400" />
-      <meta property="og:image:height" content="400" />
-      
-      {/* Twitter */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={restaurant.name} />
-      <meta name="twitter:description" content="Confira nosso cardápio digital." />
-      {absoluteLogoUrl && <meta name="twitter:image" content={absoluteLogoUrl} />}
-      
-      {/* Favicon Dinâmico */}
-      {absoluteLogoUrl && <link rel="icon" type="image/png" href={absoluteLogoUrl} />}
-    </Helmet>
-  );
-};
-
 export default RestaurantMeta;
